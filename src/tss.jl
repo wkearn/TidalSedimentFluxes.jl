505bc0eb422e2fc6ed80154c04eb1e7183ca9122
Will Kearney <wkearn@gmail.com>
2017-06-28 16:28:02 -0400

    Qualify TSS with the package name

--- src/tss.jl
+++ src/tss.jl
@@ -4,4 +4,4 @@
 Given a Turbidity time series and a fitted CalibrationModel (from TFC)
 apply the model to the Turbidity to produce a TSS.
 """
-TSS{R,S,T}(t::Turbidity{R},c::CalibrationModel{TSS{S},Turbidity{T}}) = predict(c,t)
+DischargeData.TSS{R,S,T}(t::Turbidity{R},c::CalibrationModel{TSS{S},Turbidity{T}}) = predict(c,t)

633980aeef446b82ffa61f071200d43cca16faf4
Will Kearney <wkearn@gmail.com>
2017-10-18 14:14:24 -0400

    Missed a DischargeData in the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using DischargeData, TidalSedimentFluxes, TidalFluxCalibrations, Base.Dates
+using TidalFluxQuantities, TidalSedimentFluxes, TidalFluxCalibrations, Base.Dates
 using Base.Test
 
 obs1 = OBS3("T8271",[0.0741962;0.0456691;1.40e-6],250,[-0.1695174;0.1828727;2.30e-5],1000)
